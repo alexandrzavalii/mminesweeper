b46179ecba6d1a35948fa523d1407d82b59b4da6
alexandrzavalii <alexandrzavalii@gmail.com>
2016-04-08 15:01:08 +0300

    second commit

--- client/main.js
+++ client/main.js
@@ -1,21 +1,17 @@
 import { Template } from 'meteor/templating';
-
-
 import './main.html';
+
 Template.gameBoard.onRendered(function helloOnCreated() {
   rows = 10;
   cols = 10;
   lost = false;
   clickedPlaces=[];
   placeToCheck = [[-1,-1],[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1],[-1,0]];
-  Meteor.call('createGrid',rows, cols, function(error, result){
-    Meteor.call('populateBombs', 'easy', function (error, result) {
-        placeBombs = result;
-    });
-  });
+  createGrid(rows,cols);
+  placeBombs=populateBombs('easy');
+  console.log(placeBombs.length);
 
 });
-
 
 Template.gameBoard.helpers({
   categories: function(){
@@ -26,18 +22,12 @@ Template.gameBoard.helpers({
 Template.gameBoard.events({
   "change #category-select": function (event) {
       var category = $(event.currentTarget).val();
-      Meteor.call('createGrid',rows, cols, function(error, result){
-        Meteor.call('populateBombs', category, function (error, result) {
-            placeBombs = result;
-            for(var i = 0; i<placeBombs.length; i++){
-                console.log(placeBombs[i]);
-            }
-        });
-      });
-
+      createGrid(rows,cols);
+      placeBombs=populateBombs(category);
+      console.log(placeBombs.length);
   },
   'click td'(event){
-if(lost) return ;
+  if(lost) return;
 
   //check if the place has been clicked already
   if(event.target.className.indexOf("hasBomb") > -1 || event.target.className.indexOf("noBomb") > -1){
@@ -46,77 +36,17 @@ if(lost) return ;
 //clicked cell coordinates
        var x = event.target.cellIndex;
        var y = parseInt(event.target.className);
+
 //check if the user clicked on a bomb
-       for(var i = 0; i<placeBombs.length; i++){
+      lost = checkClick(x,y);
 
-         if(x == placeBombs[i][0] && y == placeBombs[i][1]){
-             event.target.innerHTML = "X";
-             event.target.className ="bomb"
-             lost = true;
-          }
-       }
+
     if(!lost){
-        clickNext(x,y);
-        displayResult();
+        clickedPlacesThisTurn=[];
+        clickedPlacesThisTurn=clickNext(x,y);
+        displayResult(clickedPlacesThisTurn);
       }else{
-        var r= $('<center><a href="">Refresh</a></center>');
-        $("body").append(r);
+        gameover();
          }
-
-         function clickNext(x,y){
-//count the number of bombs arround cell
-           var bombsArround = 0;
-            for(i in placeToCheck){
-              for(var n = 0; n<placeBombs.length; n++){
-                if(checkArround(n,x + placeToCheck[i][0],y+ placeToCheck[i][1])){
-                  bombsArround++;
-                }
-              }
-            }
-//save to the array of clicked cells;
-        clickedPlaces[(clickedPlaces.length)] =  [x,y,bombsArround];
-//if no bombs are arround
-        if(bombsArround == 0){
-           for(var i in placeToCheck){
-//check if we are not exceeding the size of the table
-             if(x + placeToCheck[i][0] >= 0 &&
-                x + placeToCheck[i][0] <= cols-1 &&
-                y + placeToCheck[i][1] >= 0 &&
-                y + placeToCheck[i][1] <= rows-1 ){
-
-                  var x1 = x + placeToCheck[i][0];
-                  var y1 = y + placeToCheck[i][1];
-                    
-                  var alreadyClicked = false;
-                    for(n in clickedPlaces){
-                      if(clickedPlaces[n][0] == x1 && clickedPlaces[n][1] == y1){
-                         alreadyClicked = true;
-                      }
-                    }
-                    if (!alreadyClicked){
-                      clickNext(x1, y1);
-                    }
-                }
-           }
-        }
-      }
-   function checkArround(i,x,y){
-     if(placeBombs[i][0] == x && placeBombs[i][1] == y){
-        return true;
-     }else{
-       return false;
-     }
-   }
-
-   function displayResult(){
-     console.log("number of clicked places",clickedPlaces.length);
-     for(var i=0; i<clickedPlaces.length; i++){
-       if(clickedPlaces[i][2] > 0){
-         $('table.grid tr:eq(' + clickedPlaces[i][1] + ') td:eq(' + clickedPlaces[i][0] + ')').addClass( "hasBomb" ).text(clickedPlaces[i][2]);
-       }else{
-        $('table.grid tr:eq(' + clickedPlaces[i][1] + ') td:eq(' + clickedPlaces[i][0] + ')').addClass( "noBomb" );
-       }
-     }
-   }
   }
 });
---
+++ client/methods.js
@@ -0,0 +1,138 @@
+createGrid = function(rows,cols){
+
+//remove previous board
+    $('table.grid').remove();
+    grid = simpleGrid(rows,cols);
+    document.getElementById("board").appendChild(grid);
+
+    function simpleGrid ( rows, cols ){
+      var grid = document.createElement('table');
+      grid.className = 'grid';
+      for (var r=0;r<rows;++r){
+          var tr = grid.appendChild(document.createElement('tr'));
+          for (var c=0;c<cols;++c){
+              var cell = tr.appendChild(document.createElement('td'));
+                cell.className= r; //row number
+          }
+      }
+      return grid;
+    }
+};
+
+populateBombs = function(category){
+  var bombs;
+  var placeBombs = [];
+
+    if(category ==='medium'){
+      bombs = 20;
+    }else if(category==='hard'){
+      bombs = 30;
+    }else{
+      bombs = 10;
+    }
+
+    randomPosition(bombs);
+    function randomPosition(bombs) {
+            var xposition = [];
+            var yposition = [];
+            for (var i=0; i<bombs; i++) {
+                var x = Math.floor(Math.random() * 10); //generate new x point
+                var y = Math.floor(Math.random() * 10); //generate new x point
+
+                var location = xposition.indexOf(x);
+                if (location > -1) { //if newly generated x point is already in the xposition array (if it was    already previously generated
+                    if (y == yposition[location]) { //if the newly generated y points equals the same y point in  the same location as x, except in the yposition array
+                        while ( y == yposition[location]) {
+                            y = Math.floor(Math.random() * 10);
+                        }
+                    }
+                }
+                xposition.push(x);
+                yposition.push(y);
+
+                placeBombs[(placeBombs.length)] = [x,y];
+
+            }
+        }
+
+
+        return placeBombs;
+};
+
+checkClick = function(x,y){
+  for(var i = 0; i<placeBombs.length; i++){
+
+    if(x == placeBombs[i][0] && y == placeBombs[i][1]){
+        event.target.innerHTML = "X";
+        event.target.className ="bomb"
+        return true;
+     }
+  }
+  return false;
+};
+
+clickNext = function clickNext(x,y){
+//count the number of bombs arround cell
+  var bombsArround = 0;
+   for(i in placeToCheck){
+     for(var n = 0; n<placeBombs.length; n++){
+       if(checkArround(n,x + placeToCheck[i][0],y+ placeToCheck[i][1])){
+         bombsArround++;
+       }
+     }
+   }
+//save to the array of clicked cells;
+clickedPlaces[(clickedPlaces.length)] =  [x,y,bombsArround];
+clickedPlacesThisTurn[(clickedPlacesThisTurn.length)] =  [x,y,bombsArround];
+//if no bombs are arround
+if(bombsArround == 0){
+  for(var i in placeToCheck){
+//check if we are not exceeding the size of the table
+    if(x + placeToCheck[i][0] >= 0 &&
+       x + placeToCheck[i][0] <= cols-1 &&
+       y + placeToCheck[i][1] >= 0 &&
+       y + placeToCheck[i][1] <= rows-1 ){
+
+         var x1 = x + placeToCheck[i][0];
+         var y1 = y + placeToCheck[i][1];
+
+         var alreadyClicked = false;
+           for(n in clickedPlaces){
+             if(clickedPlaces[n][0] == x1 && clickedPlaces[n][1] == y1){
+                alreadyClicked = true;
+             }
+           }
+           if (!alreadyClicked){
+             clickNext(x1, y1);
+           }
+       }
+  }
+}
+return clickedPlacesThisTurn;
+};
+
+checkArround = function checkArround(i,x,y){
+  if(placeBombs[i][0] == x && placeBombs[i][1] == y){
+     return true;
+  }else{
+    return false;
+  }
+};
+
+displayResult = function displayResult(checkedPlaces){
+  console.log("looops",checkedPlaces.length);
+  for(var i=0; i<checkedPlaces.length; i++){
+    if(checkedPlaces[i][2] > 0){
+      $('table.grid tr:eq(' + checkedPlaces[i][1] + ') td:eq(' + checkedPlaces[i][0] + ')').addClass( "hasBomb" ).text(checkedPlaces[i][2]);
+    }else{
+     $('table.grid tr:eq(' + checkedPlaces[i][1] + ') td:eq(' + checkedPlaces[i][0] + ')').addClass( "noBomb" );
+    }
+  }
+}
+
+gameover = function gameover(){
+  for(var i =0; i<placeBombs.length; i++)
+      $('table.grid tr:eq(' + placeBombs[i][1] + ') td:eq(' + placeBombs[i][0] + ')').addClass( "bomb" ).text('X');
+      var r= $('<center><a href="">Refresh</a></center>');
+      $("body").append(r);
+}
